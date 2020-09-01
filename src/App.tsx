d3b71c25d1628bc9928ad2db858bdcd8a80e28fd
Rovel8 <romanovp8913@gmail.com>
2020-09-01 23:18:31 +0800

    01.09.2020 TypeScript for all

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, {ComponentType} from 'react';
 import './App.css';
+import Nav from "./parts/Nav/Nav";
+import DialogsContainer from "./parts/Dialogs/DialogsContainer";
+import {withRouter, Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
+import Music from "./parts/Music/Music";
+import News from "./parts/News/News";
+import Settings from "./parts/Settings/Settings";
+import FindUsersContainer from "./parts/FindUsers/FindUsersContainer";
+import ProfileContainer from "./parts/Body/ProfileContainer";
+import HeaderContainer from "./parts/Header/HeaderContainer";
+import Login from "./parts/Login/Login";
+import {connect, Provider} from "react-redux";
+import {compose} from "redux";
+import {initializeThunkCreator} from "./redux/app-reducer";
+import preloaderGif from './assets/loader.gif'
+import store, {AppStateType} from "./redux/redux.store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+type PropsType = MapStateType & DispatchStateType
+
+class App extends React.Component<PropsType, {}> {
+    componentDidMount()
+    {
+        this.props.initializeThunkCreator()
+    }
+    render() {
+
+        if(!this.props.initialized) {
+            return <img src={preloaderGif} alt="Loader"/>
+        }
+
+        return (
+
+            <div className='wrapper'>
+                <HeaderContainer/>
+                <Nav/>
+                <div className='content'>
+                    <Switch>
+                    <Route path="/Profile/:userId?" render={() => <ProfileContainer/>}/>
+                    <Route path="/Dialogs" render={() => <DialogsContainer/>}/>
+                    <Route path='/Music' component={Music}/>
+                    <Route path='/News' component={News}/>
+                    <Route path='/Settings' component={Settings}/>
+                    <Route path='/FindUsers' render={() => <FindUsersContainer />}/>
+                    <Route path='/Login' render={() => <Login/>}/>
+                    <Route exact path='/' render={() => <Redirect to={'/profile'}/>}/>
+                    <Route path='/*/' render={() => 'ERROR HAS OCCURRED'}/>
+                    </Switch>
+                </div>
+            </div>
+
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state: AppStateType): MapStateType => ({
+    initialized: state.app.initialized
+})
+
+type MapStateType = {
+    initialized: boolean
+}
+
+type DispatchStateType = {
+    initializeThunkCreator: () => void
+}
+
+const AppContainer =  compose<ComponentType>(withRouter,
+    connect(mapStateToProps, {initializeThunkCreator}))(App);
+
+const WholeApp = () => {
+    return (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Provider store={store}>
+                <AppContainer />
+            </Provider>
+        </BrowserRouter>
+    );
+}
+
+export default WholeApp
